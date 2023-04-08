0e57ff71187d75dae659349fd38ed9f7673796e4
gangjiang <liksyunf76@gmail.com>
2023-04-08 16:29:02 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yuanwenq/flutter_dev_utils.git main:gh-pages
+git push -f git@github.com:yuanwenq/flutter_dev_utils.git master:gh-pages
 
 cd -
